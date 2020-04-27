5904d535d3f1898cdcbacd25a7e9c9b4cdffe134
Joshua Chinemezu <joshuachinemezu@gmail.com>
2020-04-27 22:56:27 +0400

    Add loader and axios for processing request

---
+++ src/components/GlobalCount.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect } from 'react'
+// reactstrap components
+import { StatsCard, Loader } from '../components'
+import { Row, Col } from 'reactstrap'
+import { getGlobalCount } from '../store/actions'
+
+import { useDispatch, useSelector } from 'react-redux'
+
+export const GlobalCount = () => {
+	const dispatch = useDispatch()
+
+	const { globalCount } = useSelector((state) => state.stats)
+
+	useEffect(() => {
+		// Load global count on mount
+		dispatch(getGlobalCount())
+	}, [dispatch])
+
+	return (
+		<>
+			<Row className='mb-4'>
+				<Col md='3'>
+					<StatsCard
+						title={'Total Infected'}
+						number={
+							globalCount.confirmed ? (
+								globalCount.confirmed.toLocaleString()
+							) : (
+								<Loader />
+							)
+						}
+						percentage={3.48}
+						infoTest={'Since last month'}
+						icon={'fas fa-chart-bar'}
+						iconColor={'bg-warning'}
+					/>
+				</Col>
+				<Col md='3'>
+					<StatsCard
+						title={'Total Deaths'}
+						number={
+							globalCount.deaths ? (
+								globalCount.deaths.toLocaleString()
+							) : (
+								<Loader />
+							)
+						}
+						percentage={3.48}
+						infoTest={'Since last month'}
+						icon={'fas fa-chart-bar'}
+						iconColor={'bg-danger'}
+					/>
+				</Col>
+				<Col md='3'>
+					<StatsCard
+						title={'Total Recovered'}
+						number={
+							globalCount.recovered ? (
+								globalCount.recovered.toLocaleString()
+							) : (
+								<Loader />
+							)
+						}
+						percentage={3.48}
+						infoTest={'Since last month'}
+						icon={'fas fa-chart-bar'}
+						iconColor={'bg-success'}
+					/>
+				</Col>
+				<Col md='3'>
+					<StatsCard
+						title={'Total Active'}
+						number={
+							globalCount.active ? (
+								globalCount.active.toLocaleString()
+							) : (
+								<Loader />
+							)
+						}
+						percentage={3.48}
+						infoTest={'Since last month'}
+						icon={'fas fa-chart-bar'}
+						iconColor={'bg-info'}
+					/>
+				</Col>
+			</Row>
+		</>
+	)
+}
---
+++ src/components/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ClipLoader from 'react-spinners/ClipLoader'
+
+export const Loader = ({ size, color }) => {
+	return (
+		<span>
+			<ClipLoader size={size} color={color} />
+		</span>
+	)
+}
+Loader.defaultProps = {
+	size: 12,
+	color: 'black',
+}
